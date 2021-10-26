1402d52c8c4b17353c90704975ca318425c15554
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-10-26 11:03:44 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./Components/App";
+
+ReactDOM.render(
+  <>
+    <Provider>
+      <PersistGate>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </>,
+  document.getElementById("root")
+);
---
+++ src/redux/store.js
@@ -0,0 +1,4 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import logger from "redux-logger";
+import storage from "redux-persist/lib/storage";
