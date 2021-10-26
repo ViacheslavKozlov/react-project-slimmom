62950a76775abff146fb1e0924288e87f309a536
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-10-26 21:13:21 +0300

    added form logic

---
+++ src/redux/dailyRate/dailyRateReducer.js
@@ -0,0 +1,30 @@
+import { combineReducers } from "redux";
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  getDailyRateError,
+  getDailyRateRequest,
+  getDailyRateSucces,
+} from "./dailyRateActions";
+
+const dailyCaloriesReducer = createReducer(
+  {},
+  {
+    [getDailyRateSucces]: (_, action) => action.payload,
+  }
+);
+
+const dailyCaloriesErrorReducer = createReducer("", {
+  [getDailyRateError]: (_, action) => action.payload,
+});
+
+const dailyRateLoaderReducer = createReducer(false, {
+  [getDailyRateRequest]: () => true,
+  [getDailyRateSucces]: () => false,
+  [getDailyRateError]: () => false,
+});
+
+export const contactsReducer = combineReducers({
+  reponse: dailyCaloriesReducer,
+  isLoading: dailyRateLoaderReducer,
+  error: dailyCaloriesErrorReducer,
+});
