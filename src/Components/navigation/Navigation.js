b50d37fe9d9dad0911928eae0f3aff75c8a30a3a
Aleksandr Moskalyk <alex.moskalik@gmail.com>
2021-10-28 18:38:27 +0300

    add-modalNav

--- src/Components/navigation/Navigation.js
+++ src/Components/navigation/Navigation.js
@@ -1,30 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { mainRoutes } from "../../routes/mainRoutes";
 import { AuthContext } from "../App";
 import NavigationItem from "./navigationItem/NavigationItem";
 import styles from "./Navigation.module.css";
+import NavModal from "./navModal/NavModal";
+import sprite from "../../icons/sprite.svg";
 
 const Navigation = ({ routes = mainRoutes }) => {
   const [isAuth, setIsAuth] = useContext(AuthContext);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => setShowModal((prev) => !prev);
 
   const toggle = () => setIsAuth((prev) => !prev);
   return (
     <div className={styles.container}>
       <ul className={styles.listMain}>
-        {routes.map(({ name, path, exact, isPrivate, isRestricted }) => (
-          <NavigationItem
-            key={path}
-            path={path}
-            name={name}
-            exact={exact}
-            isPrivate={isPrivate}
-            isRestricted={isRestricted}
-            isAuth={isAuth}
-          />
-        ))}
+        {window.innerWidth > 767 ? (
+          routes.map(({ name, path, exact, isPrivate, isRestricted }) => (
+            <NavigationItem
+              key={path}
+              path={path}
+              name={name}
+              exact={exact}
+              isPrivate={isPrivate}
+              isRestricted={isRestricted}
+              isAuth={isAuth}
+            />
+          ))
+        ) : (
+          <>
+            {isAuth ? (
+              <li>
+                <button type="button" onClick={toggleModal}>
+                  открыть модалку
+                </button>
+              </li>
+            ) : (
+              <>
+                {routes.map(
+                  ({ name, path, exact, isPrivate, isRestricted }) => (
+                    <>
+                      {!isPrivate && (
+                        <NavigationItem
+                          key={path}
+                          path={path}
+                          name={name}
+                          exact={exact}
+                          isPrivate={isPrivate}
+                          isRestricted={isRestricted}
+                          isAuth={isAuth}
+                        />
+                      )}
+                    </>
+                  )
+                )}
+              </>
+            )}
+          </>
+        )}
 
         <li>
-          <button onClick={toggle}>isAuth</button>
+          <button onClick={toggle} type="button">
+            isAuth
+          </button>
         </li>
       </ul>
 
@@ -37,6 +76,31 @@ const Navigation = ({ routes = mainRoutes }) => {
             <span>Выйти</span>
           </li>
         </ul>
+      )}
+
+      {showModal && (
+        <NavModal toggleModal={toggleModal}>
+          {routes.map(({ name, path, exact, isPrivate, isRestricted }) => (
+            <>
+              {isPrivate && (
+                <NavigationItem
+                  key={path}
+                  path={path}
+                  name={name}
+                  exact={exact}
+                  isPrivate={isPrivate}
+                  isRestricted={isRestricted}
+                  isAuth={isAuth}
+                />
+              )}
+            </>
+          ))}
+          <button type="button" onClick={toggleModal}>
+            <svg width="20" height="20">
+              <use href={sprite + "#close"} />
+            </svg>
+          </button>
+        </NavModal>
       )}
     </div>
   );
