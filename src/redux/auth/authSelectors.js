28cadee6c0e168e7ff35ec7e2a1d048dcfa9ee26
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-10-29 11:12:58 +0200

    fixed auth selector

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,3 +1,3 @@
-export const getIsAuth = state => state.authData?.isAuth;
-export const getRefreshToken = state => state.authData.userInfo.refreshToken;
-export const getSid = state => state.authData.userInfo.sid;
+export const getIsAuth = state => state.rootReducer.authData?.isAuth;
+export const getRefreshToken = state => state.rootReducer.authData.userInfo.refreshToken;
+export const getSid = state => state.rootReducer.authData.userInfo.sid;
