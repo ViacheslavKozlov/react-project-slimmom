0a255a0b98cad8943f6495dde50be5d71a1cbab5
Serji2158 <sergey.tyschenko@gmail.com>
2021-11-02 11:54:56 +0200

    add

--- src/pages/DiaryPage.js
+++ src/pages/DiaryPage.js
@@ -4,17 +4,22 @@ import DiaryProductsList from "../Components/diaryProductsList/DiaryProductsList
 import DiaryDateСalendar from "../Components/diaryDateСalendar/DiaryDateСalendar";
 // import RightSideBar from "../Components/rightSideBar/RightSideBar";
 import DailyStatistics from "../Components/dailyStatistics/DailyStatistics";
+import useDeviceSizes from "../hooks/useDeviceSizec";
 
 const DiaryPage = () => {
   const [date, setDate] = useState(new Date());
+  const { isDescDevice } = useDeviceSizes();
 
   return (
     <>
-      <DiaryDateСalendar date={date} setDate={setDate} />
-      <DiaryAddProductForm date={date} />
-      <DiaryProductsList date={date} />
-      <DailyStatistics date={date} />
-      {/* <RightSideBar /> */}
+      <div className={isDescDevice ? "row" : "column"}>
+        <DiaryDateСalendar date={date} setDate={setDate} />
+        <DiaryAddProductForm date={date} />
+        <DiaryProductsList date={date} />
+
+        <DailyStatistics date={date} />
+        {/* <RightSideBar /> */}
+      </div>
     </>
   );
 };
